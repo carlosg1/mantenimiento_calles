fb287432895e5f18d59765eeb9800170ba31dc9e
Lic. Carlos Garcia <carlosgctes@gmail.com>
2019-03-28 11:10:26 -0300

    Agregue usuarios para login!

--- login/validar.php
+++ login/validar.php
@@ -7,8 +7,9 @@ $permitido = 'NO';
 if($username=='carlosg' && $password=='147') $permitido = 'SI';
 if($username=='dario' && $password=='147') $permitido = 'SI';
 if($username=='romina' && $password=='147') $permitido = 'SI';
-if($username=='dario' && $password=='147') $permitido = 'SI';
-if($username=='dario' && $password=='147') $permitido = 'SI';
+if($username=='mapagis' && $password=='a45r') $permitido = 'SI';
+if($username=='visor' && $password=='k537c') $permitido = 'SI';
+if($username=='ingreso' && $password=='t619w') $permitido = 'SI';
 
 if($permitido=='SI'){
 
