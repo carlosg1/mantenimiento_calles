4a2c839110d5117000d382b8d9d2a2f462279709
Carlos Garcia <carlosgctes@gmail.com>
2020-05-20 12:13:13 -0300

    Actualice algo y no me acuerdo que era

--- adds/eventos.js
+++ adds/eventos.js
@@ -3,16 +3,55 @@ $(document).ready(function(){
     overlay_CapabaseGIS_0.addTo(map);
 
     // capas de servicio publico
-    $("#chkPerfilado").click(function(ev){if(this.checked){vw_servicio_publico_perfilado.addTo(map);}else{map.removeLayer(vw_servicio_publico_perfilado);}});
-    $("#chkEnsanchamiento").click(function(ev){if(this.checked){vw_servicio_publico_ensanchamiento.addTo(map);}else{map.removeLayer(vw_servicio_publico_ensanchamiento);}});
-    $("#chkAporteSuelo").click(function(ev){if(this.checked){vw_servicio_publico_aporte_suelo.addTo(map);}else{map.removeLayer(vw_servicio_publico_aporte_suelo);}});
-    $("#chkCuneteo").click(function(ev){if(this.checked){vw_servicio_publico_cuneteo.addTo(map);}else{map.removeLayer(vw_servicio_publico_cuneteo);}});
-    $("#chkDesbarre").click(function(ev){if(this.checked){vw_servicio_publico_desbarre_de_calle.addTo(map);}else{map.removeLayer(vw_servicio_publico_desbarre_de_calle);}});
+    $("#chkPerfilado").click(function(ev){
+        if(this.checked){
+            ev.stopPropagation();
+            vw_servicio_publico_perfilado.addTo(map);
+        }else{
+            map.removeLayer(vw_servicio_publico_perfilado);
+        }
+    });
+
+    $("#chkEnsanchamiento").click(function(ev){
+        if(this.checked){
+            ev.stopPropagation();
+            vw_servicio_publico_ensanchamiento.addTo(map);
+        }else{
+            map.removeLayer(vw_servicio_publico_ensanchamiento);
+        }
+    });
+
+    $("#chkAporteSuelo").click(function(ev){
+        if(this.checked){
+            ev.stopPropagation();
+            vw_servicio_publico_aporte_suelo.addTo(map);
+        }else{
+            map.removeLayer(vw_servicio_publico_aporte_suelo);
+        }
+    });
+
+    $("#chkCuneteo").click(function(ev){
+        if(this.checked){
+            ev.stopPropagation();
+            vw_servicio_publico_cuneteo.addTo(map);
+        }else{
+            map.removeLayer(vw_servicio_publico_cuneteo);
+        }
+    });
+
+    $("#chkDesbarre").click(function(ev){
+        if(this.checked){
+            ev.stopPropagation();
+            vw_servicio_publico_desbarre_de_calle.addTo(map);
+        }else{
+            map.removeLayer(vw_servicio_publico_desbarre_de_calle);
+        }
+    });
 
     // capa alumbrado publico
     $("#chkAlumbrado").click(function(ev){
-        ev.preventDefault();
         if(this.checked){
+            ev.stopPropagation();
             vw_visor_alumbrado_publico.addTo(map);
         }else{
             map.removeLayer(vw_visor_alumbrado_publico);
@@ -21,4 +60,5 @@ $(document).ready(function(){
     });
         return true;
     // colocacion de tubos
-})
+});
+
