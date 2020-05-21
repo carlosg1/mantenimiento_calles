7d5eabda62f31d28fb99f78acb8623379399a1a3
Carlos Garcia <carlosgctes@gmail.com>
2020-05-21 08:16:15 -0300

    Agregue tubos acceso domiciliario

--- adds/eventos.js
+++ adds/eventos.js
@@ -6,11 +6,31 @@ $(document).ready(function(){
     $("#chkEnsanchamiento").click(function(ev){if(this.checked){vw_servicio_publico_ensanchamiento.addTo(map);}else{map.removeLayer(vw_servicio_publico_ensanchamiento);}});
     $("#chkAporteSuelo").click(function(ev){if(this.checked){vw_servicio_publico_aporte_suelo.addTo(map);}else{map.removeLayer(vw_servicio_publico_aporte_suelo);}});
     $("#chkCuneteo").click(function(ev){if(this.checked){vw_servicio_publico_cuneteo.addTo(map);}else{map.removeLayer(vw_servicio_publico_cuneteo);}});
-    $("#chkDesbarre").click(function(ev){if(this.checked){vw_servicio_publico_desbarre_de_calle.addTo(map);}else{map.removeLayer(vw_servicio_publico_desbarre_de_calle);}});
+    
+    $("#chkDesbarre").click(function(ev){
+        if(this.checked){
+            vw_servicio_publico_desbarre_de_calle.addTo(map);
+        }else{
+            map.removeLayer(vw_servicio_publico_desbarre_de_calle);
+        }
+    });
 
+    // alumbrado
+    $("#chkAlumbrado").click(function(ev){
+        if(this.checked){
+            vw_visor_alumbrado_publico.addTo(map);
+        }else{
+            map.removeLayer(vw_visor_alumbrado_publico);
+        }
+    });
 
+    // acceso domiciliario
+    $("#chkTuboAccesoDomicilio").click(function(ev){
+        if(this.checked){
+            vw_visor_colocacion_tubo_acdom.addTo(map);
+        }else{
+            map.removeLayer(vw_visor_colocacion_tubo_acdom);
+        }
+    });
 
-
-
-    $("#chkAlumbrado").click(function(ev){if(this.checked){vw_visor_alumbrado_publico.addTo(map);}else{map.removeLayer(vw_visor_alumbrado_publico);}});
-})
+});
